9ea53be7269ee6af786a16dd3c9595d9e5072d18
Code Bridge Machine <ahmed@codebridge.my>
2025-04-25 09:43:16 +0800

    CodeBridge commit

---
+++ nodejs-backend/src/models/products.model.js
@@ -0,0 +1,25 @@
+
+    module.exports = function (app) {
+        const modelName = 'products';
+        const mongooseClient = app.get('mongooseClient');
+        const { Schema } = mongooseClient;
+        const schema = new Schema(
+          {
+            name: { type:  String , required: true },
+sku: { type: String, required: false , enum: ["A","B","C","D"] },
+
+            
+            createdBy: { type: Schema.Types.ObjectId, ref: "users", required: true },
+            updatedBy: { type: Schema.Types.ObjectId, ref: "users", required: true }
+          },
+          {
+            timestamps: true
+        });
+      
+       
+        if (mongooseClient.modelNames().includes(modelName)) {
+          mongooseClient.deleteModel(modelName);
+        }
+        return mongooseClient.model(modelName, schema);
+        
+      };
